cc151421eb3566cbbecded5dc2c03ea323190ced
Kosar Hosseinkhani <kosarh79@gmail.com>
2016-12-09 15:26:05 -0800

    AWS Lamda Notification to Sonicspan after image conversion

--- index.js
+++ index.js
@@ -2,18 +2,56 @@
 var async = require('async');
 var path = require('path');
 var AWS = require('aws-sdk');
+var https = require('https');
+var querystring = require('querystring');
 var gm = require('gm').subClass({
     imageMagick: true
 });
 var util = require('util');
 // get reference to S3 client
 var s3 = new AWS.S3();
+var querystring = require('querystring');
+var notifySonicspan = function(filename){
+    console.log('~~~~~~~ Notfying Sonicspan ......');
+    var post_data = querystring.stringify({
+        'email' : process.env.email,
+        'password': process.env.password,
+        'filename': filename
+    });
+    var options = {
+        host: "malayerdev.herokuapp.com",
+        path: "/api/aws/trans",
+        port:443,
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(post_data)
+        }
+    };
+    var req = https.request(options, function(res) {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+            context.done();
+        });
+
+    });
+    req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+    });
+    // post the data
+    req.write(post_data);
+    req.end();
+};
 exports.handler = function(event, context) {
     // Read options from the event.
     // console.log("Reading options from event:\n", util.inspect(event, {
     //     depth: 5
     // }));
     var srcBucket = event.Records[0].s3.bucket.name;
+    var fileName;
     // Object key may have spaces or unicode non-ASCII characters.
     var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(
         /\+/g, " "));
@@ -25,16 +63,19 @@ exports.handler = function(event, context) {
     //  }
     var large = {
         width: 1440,
+        height:927,
         dstnKey: srcKey,
         destinationPath: "large"
     };
     var medium = {
         width:866.6,
+        height:668,
         dstnKey: srcKey,
         destinationPath: "medium"
     };
     var small = {
         width: 474.6,
+        height:320,
         dstnKey: srcKey,
         destinationPath: "small"
     };
@@ -51,7 +92,7 @@ exports.handler = function(event, context) {
     // console.log(srcKey);
     // Infer the image type.
     var typeMatch = srcKey.match(/\.([^.]*)$/);
-    var fileName = path.basename(srcKey);
+    fileName = path.basename(srcKey);
     if (!typeMatch) {
         console.error('unable to infer image type for key ' + srcKey);
         return;
@@ -67,8 +108,8 @@ exports.handler = function(event, context) {
         async.waterfall([
 
             function download(next) {
-              //  console.time("downloadImage");
-              //  console.log("download");
+                //  console.time("downloadImage");
+                //  console.log("download");
                 // Download the image from S3 into a buffer.
                 // sadly it downloads the image several times, but we couldn't place it outside
                 // the variable was not recognized
@@ -76,14 +117,14 @@ exports.handler = function(event, context) {
                     Bucket: srcBucket,
                     Key: srcKey
                 }, next);
-              //  console.timeEnd("downloadImage");
+                //  console.timeEnd("downloadImage");
             },
             function convert(response, next) {
                 // convert eps images to png
-               // console.time("convertImage");
+                // console.time("convertImage");
                 //console.log("Reponse content type : " +
-                   // response.ContentType);
-               // console.log("Conversion");
+                // response.ContentType);
+                // console.log("Conversion");
                 gm(response.Body).antialias(true).density(
                     300).toBuffer('JPG', function(err,
                                                   buffer) {
@@ -91,16 +132,16 @@ exports.handler = function(event, context) {
                         //next(err);
                         next(err);
                     } else {
-                      //  console.timeEnd(
-                       //     "convertImage");
+                        //  console.timeEnd(
+                        //     "convertImage");
                         next(null, buffer);
                         //next(null, 'done');
                     }
                 });
             },
             function process(response, next) {
-             //   console.log("process image");
-              //  console.time("processImage");
+                //   console.log("process image");
+                //  console.time("processImage");
                 // Transform the image buffer in memory.
                 //gm(response.Body).size(function(err, size) {
                 gm(response).autoOrient().size(function(err, size) {
@@ -118,8 +159,7 @@ exports.handler = function(event, context) {
                     }
                     else{
                         scalingFactor = Math.min(
-                            _sizesArray[key].width / size.width, _sizesArray[key].width / size.height
-                        );
+                            _sizesArray[key].width / size.width, _sizesArray[key].height / size.height, 1);
 
                         width = scalingFactor * size.width;
                         height = scalingFactor * size.height;
@@ -133,9 +173,9 @@ exports.handler = function(event, context) {
                                 //next(err);
                                 next(err);
                             } else {
-                              //  console.timeEnd(
-                              //      "processImage"
-                              //  );
+                                //  console.timeEnd(
+                                //      "processImage"
+                                //  );
                                 next(null,
                                     buffer,
                                     key);
@@ -145,12 +185,12 @@ exports.handler = function(event, context) {
                 });
             },
             function upload(data, index, next) {
-               // console.time("uploadImage");
-              //  console.log("upload : " + index);
-              //  console.log("upload to path : /images/" +
-                 //   _sizesArray[index].destinationPath +
-                 //   "/" + fileName.slice(0, -4) +
-                 //   ".jpg");
+                // console.time("uploadImage");
+                //  console.log("upload : " + index);
+                //  console.log("upload to path : /images/" +
+                //   _sizesArray[index].destinationPath +
+                //   "/" + fileName.slice(0, -4) +
+                //   ".jpg");
                 // Stream the transformed image to a different folder.
                 s3.putObject({
                     Bucket: dstBucket,
@@ -163,7 +203,7 @@ exports.handler = function(event, context) {
                     Body: data,
                     ContentType: 'JPG'
                 }, next);
-              //  console.timeEnd("uploadImage");
+                //  console.timeEnd("uploadImage");
             }
         ], function(err, result) {
             if (err) {
@@ -182,6 +222,7 @@ exports.handler = function(event, context) {
             console.log('---->Successfully resized ' + srcBucket +
                 ' and uploaded to' + dstBucket + "/images");
         }
-        context.done();
+        notifySonicspan(fileName, context);
+
     });
 };
